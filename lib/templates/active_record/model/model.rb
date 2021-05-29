2b1ad24804d008db47051d014a11d70691c04d2f
hatsu38 <hajiwata0308@gmail.com>
2021-05-29 13:48:56 +0900

    Add annotate model template

---
+++ lib/templates/active_record/model/model.rb
@@ -0,0 +1,17 @@
+<% module_namespacing do -%>
+class <%= class_name %> < <%= parent_class_name.classify %>
+  include Auditable
+
+  # Relations
+
+  # Validations
+
+  # Callbacks
+
+  # Delegates
+
+  # Scopes
+
+  # Methods
+end
+<% end -%>
