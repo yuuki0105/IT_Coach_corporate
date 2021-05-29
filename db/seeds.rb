f54464b0a8238bbcb13f5f159c12d93a2317e1a8
hatsu38 <hajiwata0308@gmail.com>
2021-05-29 15:44:52 +0900

    Add seeds skills

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+skill_categories = [
+  {category_name: "SNSマーケ", skills: ["FB", "Twitter"]},
+  {category_name: "EC", skills: ["shopify"]},
+  {category_name: "Webマーケ", skills: ["seo"]},
+  {category_name: "Web制作", skills: ["wp", "nocode"]}
+]
+
+skill_categories.each do |skill_category|
+  category = SkillCategory.find_or_create_by(name: skill_category[:category_name])
+  skill_category[:skills].each do |skill|
+    category.skills.find_or_create_by(name: skill)
+  end
+end
