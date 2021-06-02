d01ef2b284cc6e7f66be0d0bf3c75d892ec9cb55
github yuuki0105 <komatsu3613@gmail.com>
2021-06-02 11:42:50 +0900

    シードファイルの変更（カテゴリー登録）

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,10 +13,14 @@ end
 
 
 big_categories = [
-  {big_category: "SNSマーケティング", categories: ["Facebook", "Twitter", "Instagram", "LINE", "Youtube", "その他"]},
-  {big_category: "EC・ネットショップ", categories: ["Amazon", "楽天", "Yahooショッピング", "Shopify"]},
-  {big_category: "Webマーケティング", categories: ["SEO", "リスティング"]},
-  {big_category: "Web制作・開発", categories: ["Wordpress", "nocode"]}
+
+  {big_category: "SNSマーケティング", categories: ["戦略", "企画", "分析", "売上アップ", "コスト削減", "集客", "内製化・インハウス", "アカウント運用", "広告運用", "ブランディング", "インフルエンサーマーケティング", "撮影・写真", "画像・サムネイル・バナー", "コピー・ライティング", "Facebook", "Instagram", "Twitter", "LINE", "Youtube", "Tiktok", "Pinterest", "その他"]},
+
+  {big_category: "EC・ネットショップ", categories: ["戦略", "企画", "分析", "売上アップ", "コスト削減", "集客", "内製化・インハウス", "ECサイト構築・制作", "ECサイト運用・運営", "撮影・写真", "画像・サムネイル・バナー", "コピー・ライティング", "決済システム", "Webマーケティング", "SNSマーケティング", "インフルエンサーマーケティング", "広告運用", "ブランディング", "D2C","Amazon", "楽天", "Yahoo!ショッピング", "Shopify", "BASE", "STORES", "WordPress", "EC-CUBE", "カラーミーショップ", "MakeShop", "Stripe", "Instagram", "その他"]},
+
+  {big_category: "Webマーケティング", categories: ["戦略", "企画", "分析", "売上アップ", "コスト削減", "集客", "内製化・インハウス", "サイト構築・制作", "サイト運用・運営", "撮影・写真", "画像・サムネイル・バナー", "コピー・ライティング", "広告運用", "SEO", "MEO", "リスティング", "リターゲティング", "ディスプレイ", "アドネットワーク", "ブログ", "オウンドメディア", "アフィリエイト", "ホームページ", "Webサイト", "LP・ランディングページ", "動画マーケティング", "メールマーケティング", "アクセス解析", "Google", "Google AdWords", "Google Map", "Googleマイビジネス", "Googleアナリティクス", "Yahoo!JAPAN", "その他"]},
+
+  {big_category: "Web制作", categories: ["戦略", "企画", "分析", "売上アップ", "コスト削減", "集客", "内製化・インハウス", "UIUX", "ワイヤーフレーム・モックアップ",  "サイト構築・制作", "サイト運用・運営", "ロゴ", "撮影・写真", "画像・サムネイル・バナー", "コピー・ライティング", "ホームページ", "Webサイト", "LP・ランディングページ", "コーディング・マークアップ", "サーバー", "ドメイン", "データベース", "問合せ・予約・注文フォーム", "Illustrator", "Photoshop", "Adobe XD", "Figma", "WordPress", "Wix", "STUDIO", "HTML・CSS", "その他"]}
 ]
 
 big_categories.each do |big_category|
